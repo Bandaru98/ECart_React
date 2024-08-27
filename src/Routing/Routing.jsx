efa656fc2d410996aaf550e526fa1eea1dc90296
Bandaru98 <bandarumadhavi681998@gmail.com>
2024-08-27 22:44:05 +0530

    update details

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -4,6 +4,10 @@ import Home from '../Components/Home/Home'
 import Cart from '../Components/Cart/Cart'
 import Singlepage from '../Components/Singlepage/Singlepage'
 import Fetchdata from '../Components/Fetchdata/Fetchdata'
+import Login from '../Components/Login/Login'
+import Register from '../Components/Register/Register'
+import About from '../Components/About/About'
+import Contact from '../Components/Contact/Contact'
 
 
 const Routing = () => {
@@ -12,12 +16,21 @@ const Routing = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/products' element={<Fetchdata />} />
-                <Route path='/cart' element={<Cart/>} />
-                <Route path='/product/:id' element={<Singlepage/>} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/product/:id' element={<Singlepage />} />
+
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register/>} />
+                <Route path='/about' element={<About/>} />
+                <Route path='/contact' element={<Contact/>} />
 
 
-               
-            
+
+
+
+
+
+
 
             </Routes>
         </>
